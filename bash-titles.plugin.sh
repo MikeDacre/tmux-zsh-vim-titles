e6d6c2432cf057dadcd0b96b5c0f253f9223b43e
Mike Dacre <mike.dacre@gmail.com>
2018-03-16 15:40:43 -0700

    Include hostname in bash title

--- bash-titles.plugin.sh
+++ bash-titles.plugin.sh
@@ -2,13 +2,13 @@
 
 # Set title with PS1 to be 'b:<path>'
 if [[ -n "$TMUX" ]]; then
-    PS1="\ekb:\w\e\a$PS1"
-    PS1="\033]0;b:\w\a$PS1"
+    PS1="\ekb:${HOSTNAME}:\w\e\a$PS1"
+    PS1="\033]0;b${HOSTNAME}::\w\a$PS1"
 elif [[ $TERM =~ screen* ]]; then
-    PS1="\ekb:\w\e\a$PS1"
-    PS1="\033]0;b:\w\a$PS1"
+    PS1="\ekb${HOSTNAME}::\w\e\a$PS1"
+    PS1="\033]0;b:${HOSTNAME}:\w\a$PS1"
 elif [[ $TERM =~ xterm* ]]; then
-    PS1="\033]0;b:\w\a$PS1"
+    PS1="\033]0;b:${HOSTNAME}:\w\a$PS1"
 elif [[ $TERM =~ ^rxvt-unicode.* ]]; then
-    PS1="\33]2;b:\w\007$PS1"
+    PS1="\33]2;b:${HOSTNAME}:\w\007$PS1"
 fi
